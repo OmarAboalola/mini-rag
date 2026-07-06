9b7dc2aba88bd1daf905933f5262daea4072afb7
OmarAboalola <92317542+OmarAboalola@users.noreply.github.com>
2026-07-06 17:22:44 +0300

    update docker settings

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -7,6 +7,27 @@ class ProjectModel(BaseDataModel):
     def __init__(self, db_client: object):
         super().__init__(db_client=db_client)
         self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
+        #remember when dealing with mongo we use asynico as the motor lib is async, so we need to use await when calling the methods of the motor lib, and we need to use async def when defining the methods of this class
+    @classmethod
+     #fixes the problem of having an async init method, because we cannot have an async init method, so we use a class method to create an instance of the class and then call the async init method
+     ##check the video for more info: https://youtu.be/iO8FAmUVcjE?si=BmbOifTQX24z2hHU&t=1875
+    async def create_instance(cls, db_client: object):
+        instance = cls(db_client)
+        await instance.init_collection()
+        return instance
+
+    async def init_collection(self):
+        all_collections = await self.db_client.list_collection_names()
+        if DataBaseEnum.COLLECTION_PROJECT_NAME.value not in all_collections:
+            self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
+            indexes = Project.get_indexes()
+            for index in indexes:
+                await self.collection.create_index(
+                    index["key"],
+                    name=index["name"],
+                    unique=index["unique"]
+                )
+
 
     async def create_project(self, project: Project):
 
--- src/models/db_schemes/data_chunk.py
+++ src/models/db_schemes/data_chunk.py
@@ -1,14 +1,26 @@
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field , validator
 from typing import Optional
-from bson.objectid import ObjectId
+from bson import ObjectId
 
+#data chunk db table schema
 class DataChunk(BaseModel):
     id: Optional[ObjectId] = Field(None, alias="_id")
     chunk_text: str = Field(..., min_length=1)
     chunk_metadata: dict
-    chunk_order: int = Field(..., gt=0)
-    chunk_project_id: ObjectId
-
+    chunk_order: int = Field(..., gt=0)#gt stands for greater than, so this means that the chunk_order must be greater than 0
+    chunk_project_id: ObjectId  # originally --> #chunk_project_id:bson.ObjectId.ObjectId 
+#this chunk belongs to a specific project , so we have the project_id as a foreign key to the project table
     class Config:
         arbitrary_types_allowed = True
+         #pydantic by default does not support ObjectId, 
+         # so we need to allow arbitrary (ignore them)types
 
+    @classmethod
+    def get_indexes(cls): # the key word self refers to an instance while cls refers to the class itself, so we use cls here because we are defining a class method ( no need to create an instance of the class to use this method)
+        return [
+        {
+            "key": [("project_id", 1)],#[...,1] means ascending order, [-1] means descending order
+            "name": "project_id_index_1",
+            "unique": False #this index is not unique because a project can have multiple chunks
+        }
+    ]
