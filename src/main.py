fcefab39f7b6dea8b0c8c89a11bcc3dac351cc61
OmarAboalola <92317542+OmarAboalola@users.noreply.github.com>
2026-07-10 19:43:27 +0300

    add factory pattern to main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+from stores.LLM.LLMProviderFactory import LLMPROVIDERFACTORY
 
 app = FastAPI()
 
@@ -11,6 +12,17 @@ async def startup_db_client():
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
+    #==============Factory pattern for LLM providers========================
+    llm_provider_factory= LLMPROVIDERFACTORY(settings)
+
+
+    #generation client 
+    app.generation_client = llm_provider_factory.create_provider(provider_name=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+    #embedding client
+    app.embedding_client = llm_provider_factory.create_provider(provider_name=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID, embedding_size=settings.EMBEDDING_MODEL_SIZE)
+    
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongo_conn.close()
