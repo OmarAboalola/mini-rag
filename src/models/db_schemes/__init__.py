84367ddabf9130a0d70b2d57f00e309dc8726544
OmarAboalola <92317542+OmarAboalola@users.noreply.github.com>
2026-07-20 19:34:47 +0300

    Refactor database schema to use SQLAlchemy with Alembic migrations

--- src/models/db_schemes/__init__.py
+++ src/models/db_schemes/__init__.py
@@ -1,3 +1,1 @@
-from .project import Project
-from .data_chunk import DataChunk , RetrivedDocument
-from .asset import Asset
+from .minirag.schemes import SQLAlchemyBase ,Asset,Project,DataChunk, RetrievedDocument
---
+++ src/models/db_schemes/minirag/schemes/datachunk.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from .minirag_base import SQLAlchemyBase
+from sqlalchemy import Column ,Integer , String ,DateTime ,func , ForeignKey
+from sqlalchemy.dialects.postgresql import UUID ,JSONB
+from sqlalchemy import Index
+from sqlalchemy.orm import relationship
+import uuid
+
+class DataChunk (SQLAlchemyBase):
+    __tablename__="chunks"
+
+    chunk_id=Column(Integer,primary_key=True,autoincrement=True)
+    chunk_uuid=Column(UUID(as_uuid=True) ,default=uuid.uuid4 , unique=True , nullable=False) #generate a unique value and cannot be null
+
+    chunk_text=Column(String,nullable=False)
+    chunk_metadata=Column(JSONB,nullable=True)
+    chunk_order=Column(Integer,nullable=False)
+    
+    #this chunk belongs to what project (table)--> (fk)
+
+    chunk_project_id=Column(Integer,ForeignKey("projects.project_id"),nullable=False)
+
+    chunk_asset_id=Column(Integer,ForeignKey("assets.asset_id"),nullable=False)
+
+    
+    created_at=Column(DateTime(timezone=True) , server_default=func.now() , nullable=False) #auto add it if not found and cannot be null
+    updated_at=Column(DateTime(timezone=True) , onupdate=func.now() , nullable=True)  #can be null if col is not updated
+
+    #2 relationships as we have 2 foreign keys
+    project = relationship("Project",back_populates="chunks")
+    asset=relationship("Asset",back_populates="chunks")
+
+    __table_args__=(
+        Index('ix_chunk_project_id',chunk_project_id),
+        Index("ix_chunk_asset_id", chunk_asset_id),
+    )
+class RetrievedDocument(BaseModel):
+    text:str
+    score:float
+
